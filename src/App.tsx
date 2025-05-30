c78b5ca2362ec56496421b09eab34b9f7e3fc852
Ricardo Alexander <ricardoalexanderh@gmail.com>
2025-05-30 10:50:06 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MentalArithmeticGame from './components/mental-arithmetic-game'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <MentalArithmeticGame />
+    </div>
+  )
+}
+
+export default App
